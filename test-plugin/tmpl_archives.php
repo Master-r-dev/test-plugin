a293f43302a77b06592ef0486b612be7306b7d7d
Alex S <71697746+Master-r-dev@users.noreply.github.com>
2023-06-29 22:14:14 +0400

    Update tmpl_archives.php
    Forgot to upload update for custom field

--- test-plugin/tmpl_archives.php
+++ test-plugin/tmpl_archives.php
@@ -40,8 +40,9 @@ function showResult(str) {
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
 	  text=JSON.parse(this.responseText); 
+    document.getElementById("livesearch").innerHTML="";
 	  for (let x in text){
-		document.getElementById("livesearch").innerHTML+=text[x]['post_name']+'   |   ';
+		  document.getElementById("livesearch").innerHTML+=text[x]['post_name']+'   |   ';
 	  }      
       document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     }
